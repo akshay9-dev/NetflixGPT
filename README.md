0fb95ff552a8c69647dca7fe5b923da40562737f
akshays999 <akshayshahane1721@gmail.com>
2025-03-04 23:02:45 +0530

    validation and signIn/signOut and redux store

--- README.md
+++ README.md
@@ -3,9 +3,19 @@
 - Installed vite: npm create vite@latest
 - Configured Tailwind CSS
 - Header
+- Routing of app
 - Login Form
 - Sign up form
-
+- Form validation
+- useRef Hook 
+- firebase setup
+- Create a signup user account
+- Implement Sign In user API
+- Created Redux store with slice user
+- Implemented sign out
+- update profile
+- fetch movie from TMDB. 
+ 
 # Features
 
 - Login/Sign up
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,92 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch= useDispatch();
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //Validate the form data
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      isSignInForm ? null : name.current?.value
+    );
+    setErrorMessage(message);
+
+    if (message) return;
+
+    //Sign In/ Sign Up logic
+    if (!isSignInForm) {
+      //Sign Up logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+          })
+            .then(() => {
+              const { uid, email, displayName, } = auth.currentUser;
+        dispatch(
+          addUser ({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+          })
+        );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(errorCode + "-" + errorMessage);
+            });
+
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //Sign In logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -17,28 +101,36 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="w-8/24 absolute p-12 bg-black/80 mt-36 mx-auto right-0 left-0 text-white bg-o">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-8/24 absolute p-12 bg-black/80 mt-36 mx-auto right-0 left-0 text-white bg-o">
         <h1 className="font-bold text-3xl py-4 ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 bg-gray-700 w-full rounded"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 bg-gray-700 w-full rounded"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 bg-gray-700 w-full rounded"
         />
-        <button className="p-4 my-8  bg-red-700 w-full rounded">
+        <p className="text-red-600 font-bold text-lg">{errorMessage}</p>
+        <button
+          className="p-4 my-8  bg-red-700 w-full rounded"
+          onClick={handleButtonClick}>
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
