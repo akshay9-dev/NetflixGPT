d1e5bd7e1e1b15c9486a93eb2c058e3619631dbc
akshays999 <akshayshahane1721@gmail.com>
2025-03-09 17:53:34 +0530

    made id fetch dynamic

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -11,12 +11,12 @@ const MainContainer = () => {
   const mainMovie = movies[0];
   console.log(mainMovie);
 
-  const { original_title, overview } = mainMovie;
+  const { original_title, overview, id } = mainMovie;
 
   return (
     <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground movieId={950396} />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
