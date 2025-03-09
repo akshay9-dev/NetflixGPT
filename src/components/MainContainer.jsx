ced53ff4e9abbfb283af148ec9c160722894b18c
akshays999 <akshayshahane1721@gmail.com>
2025-03-09 17:41:36 +0530

    made changes in API

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,15 +8,15 @@ const MainContainer = () => {
 
   if (!movies) return;
 
-  const mainMovie = movies[18];
-  // console.log(mainMovie);
+  const mainMovie = movies[0];
+  console.log(mainMovie);
 
   const { original_title, overview } = mainMovie;
 
   return (
     <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground movieId={549509} />
+      <VideoBackground movieId={950396} />
     </div>
   );
 };
